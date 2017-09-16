24fc69179d3e38cb558e7e163f00717a256c5383
liuqiong0139 <1141787559@qq.com>
2017-09-16 22:52:36 +0800

    update extends code

--- learning.java.for.example/src/day05/StaticCodeDemo.java
+++ learning.java.for.example/src/day05/StaticCodeDemo.java
@@ -4,7 +4,23 @@ public class StaticCodeDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StaticCode.print();
+		// StaticCode.print();
+
+		String st = "abcddcaa";
+		boolean result = testDemo(st);
+		System.out.println(result);
+	}
+
+	// 比较string是否对称
+	public static boolean testDemo(String st) {
+		char[] charArray = st.toCharArray();
+		int min = 0;
+		int max = charArray.length - 1;
+		while (max >= min) {
+			if (!(charArray[min++] == charArray[max--]))
+				return false;
+		}
+		return true;
 	}
 
 }
